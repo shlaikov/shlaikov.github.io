0d947c3f061e9aab2b7905c4537de2e535eaa11b
shlaikov <shlaikov.alexey@ya.ru>
2017-04-09 04:32:25 +0300

    production.

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,26 @@
-$(document).ready(function () {
-    $(".element").typed({
-        strings: [
-            "Добро пожаловать на мой сайт!",
-            "Приятно познакомиться с вами.",
-            "Я web-разработчик",
-            "Алексей Шлайков."
-        ],
-        startDelay: 0.5,
-        typeSpeed: 0.1,
-        backSpeed: 0.9,
-        backDelay: 700,
+jQuery(document).ready(function ($) {
 
-        callback: function () {
-            $(".discriptor").show("fast");
-            $(".main-body").show("fast");
-            $(".avatar").show("fast");
-        }
+    $(window).on('load', function () {
+        $('.preloader').fadeOut(1000, function () {
+            $('.preloader').remove();
+
+            $(".element").typed({
+                strings: [
+                    "Добро пожаловать на мой сайт!",
+                    "Я web-разработчик",
+                    "Алексей Шлайков."
+                ],
+                startDelay: 0.5,
+                typeSpeed: 0.1,
+                backSpeed: 0.9,
+                backDelay: 700,
+
+                callback: function () {
+                    $(".discriptor").show("fast");
+                    $(".main-body").show("fast");
+                    $(".avatar").show("fast");
+                }
+            });
+        });
     });
 });
